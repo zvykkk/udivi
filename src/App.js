3d619da1e983871e2fd17423c7b18f2e9b7348fa
Anna Tolokonnikova <zzz.moi.zzz.moi@gmail.com>
2020-07-30 13:53:22 +0300

    add router

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-
-function App() {
-  return <div className="App">welcome</div>;
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from "react-router-dom";
 
-import App from './App';
+import Routes from 'routes'
+
+const App = () => {
+    return (
+        <div>
+            <h3>Welcome to hooks</h3>
+            <Router>
+             <Routes/>
+            </Router>
+        </div>
+    )
+}
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
