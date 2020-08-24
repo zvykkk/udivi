58666f8614a9cddde1ab7394fe1050d126215975
Anna Tolokonnikova <zzz.moi.zzz.moi@gmail.com>
2020-08-24 17:08:08 +0300

    add register

--- src/pages/authentication/index.js
+++ src/pages/authentication/index.js
@@ -1,43 +1,53 @@
 import React, {useState} from 'react'
 import {Link} from "react-router-dom";
 
-
 import useFetch from "../../hooks/useFetch";
 
-const Authentication = () => {
+const Authentication = props => {
+    const isLogin = props.match.path === '/login'
+    const pageTitle = isLogin ? 'Sign In' : 'Sign Up'
+    const descriptionText = isLogin ? 'Need an account?' : 'Have an account?'
+    const descriptionLink = isLogin ? '/register' : '/login'
+    const apiUrl = isLogin ? '/users/login' : '/users'
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const [{isLoading, response, error}, doFetch] = useFetch('/users/login')
+    const [username, setUsername] = useState('')
+    const [{isLoading, response, error}, doFetch] = useFetch(apiUrl)
 
 
     const handleSubmit = event => {
         event.preventDefault()
+        const user = isLogin ? {email, password} : {email, password, username}
         doFetch({
                 method: 'post',
                 data: {
-                    user: {
-                        email: 'xcxc@hg.com',
-                        password: '123'
-                    }
+                    user
                 }
             })
     }
-
-
-
-
 
     return (
         <div className='auto-page'>
             <div className='container page'>
                 <div className='row'>
                     <div className='col-md-6 offset-md-3 col-xs-12'>
-                        <h1 className='text-center'>Login</h1>
+                        <h1 className='text-center'>{pageTitle}</h1>
                         <p className='text-center'>
-                            <Link to='register'>Need an account?</Link>
+                            <Link to={descriptionLink}>{descriptionText}</Link>
                         </p>
                         <form onSubmit={handleSubmit}>
                             <fieldset>
+                                {!isLogin && (
+                                    <fieldset className='form-group'>
+                                        <input
+                                            type='text'
+                                            className='form-control form-control-lg'
+                                            placeholder='Username'
+                                            value={username}
+                                            onChange={e => setUsername (e.target.value)}
+                                        />
+                                    </fieldset>
+                                )}
                                 <fieldset className='form-group'>
                                     <input
                                         type='email'
@@ -61,7 +71,7 @@ const Authentication = () => {
                                     type='submit'
                                     disabled={isLoading}
                                 >
-                                    Sign in
+                                    {pageTitle}
                                 </button>
                             </fieldset>
                         </form>
